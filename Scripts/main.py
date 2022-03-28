4c99994ed0ad30b17574b1f8bbb07160c5263c4f
Pablo Rees <pablorees01@gmail.com>
2022-03-28 12:33:55 +0200

    Rearranged Main file to be more functional

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,16 +6,7 @@ from Word2Vec import *
 from fin_data_downloader import *
 from R_Interface import *
 from Data_congregator import *
-
-############# Setting up text data #############
-speechDataSavePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Speech_data'
-
-runWSC(speechDataSavePath) #calls the WebScrapeAndClean process - text data
-
-Word2Vec_Model = trainWord2Vec(speechDataSavePath) #creates the word2vec model - text data
-
-featuresFilePath = "/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Speech_data_with_features/"
-liteFeaturesFilesPath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Speech_data_lite/'
+from Transpose import *
 
 def textPrep(speechDataSavePath,featuresFilePath,liteFeaturesFilesPath):
 
@@ -54,32 +45,58 @@ def textPrep(speechDataSavePath,featuresFilePath,liteFeaturesFilesPath):
                 featuresFilePath + df_name,
                 sep='\t')
 
-textPrep(speechDataSavePath, featuresFilePath, liteFeaturesFilesPath)
+############# Setting up text data #############
+def setUpTextData():
+    speechDataSavePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Speech_data'
 
-completeDataFilePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Complete_data'
+    runWSC(speechDataSavePath)  # calls the WebScrapeAndClean process - text data
 
-combinedLiteSpeeches = combineSpeeches(liteFeaturesFilesPath)
-combinedLiteSpeeches.to_csv(completeDataFilePath + '/combinedLiteSpeeches.tsv', sep='\t')
+    Word2Vec_Model = trainWord2Vec(speechDataSavePath)  # creates the word2vec model - text data
 
-############# Setting up financial data #############
-financialDataSavePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/GSPC.csv'
-downloadFinData(financialDataSavePath,'^GSPC',1927, 12, 30, 23, 59)
+    featuresFilePath = "/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Speech_data_with_features/"
+    liteFeaturesFilesPath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Speech_data_lite/'
 
-############ Download Financial Metadata ##############
-metadataFilePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/MetaData/'
+    textPrep(speechDataSavePath, featuresFilePath, liteFeaturesFilesPath)
 
-downloadFinData(metadataFilePath + 'USDX.csv','DX-Y.NYB',1971, 1, 3, 23, 59)
-downloadFinData(metadataFilePath + 'BTC.csv','BTC-USD',2014, 9, 16, 23, 59)
-downloadFinData(metadataFilePath + 'VIX.csv','^VIX',1990, 1, 1, 23, 59)
-downloadFinData(metadataFilePath + 'OIL.csv','CL%3DF',2000, 8, 22, 23, 59)
-downloadFinData(metadataFilePath + 'NASDAQ_comp.csv','%5EIXIC',1971, 2, 4, 23, 59)
-downloadFinData(metadataFilePath + 'SSE_comp.csv','000001.SS',1997, 7, 1, 23, 59)
+    completeDataFilePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Complete_data'
+
+    combinedLiteSpeeches = combineSpeeches(liteFeaturesFilesPath)
+    combinedLiteSpeeches.to_csv(completeDataFilePath + '/combinedLiteSpeeches.tsv', sep='\t')
+
+############# Setting up financial data and metadata #############
+def setUpFinancialData():
+    financialDataSavePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/GSPC.csv'
+    downloadFinData(financialDataSavePath, '^GSPC', 1927, 12, 30, 23, 59)
+
+    metadataFilePath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/MetaData/'
+
+    downloadFinData(metadataFilePath + 'USDX.csv', 'DX-Y.NYB', 1971, 1, 3, 23, 59)
+    downloadFinData(metadataFilePath + 'BTC.csv', 'BTC-USD', 2014, 9, 16, 23, 59)
+    downloadFinData(metadataFilePath + 'VIX.csv', '^VIX', 1990, 1, 1, 23, 59)
+    downloadFinData(metadataFilePath + 'OIL.csv', 'CL%3DF', 2000, 8, 22, 23, 59)
+    downloadFinData(metadataFilePath + 'NASDAQ_comp.csv', '%5EIXIC', 1971, 2, 4, 23, 59)
+    downloadFinData(metadataFilePath + 'SSE_comp.csv', '000001.SS', 1997, 7, 1, 23, 59)
 
 ######run R files - GSPC_TS , metadata_setup and combining final data
-R_call('GSPC_timeseries.R')
-R_call('metaDataSetup.R')
-R_call('combine_by_date.R')
+def runR_files():
+    R_call('GSPC_timeseries.R')
+    R_call('metaDataSetup.R')
+    R_call('combine_by_date.R')
 
+###### Transpose vectorized speech variables and sentiment vectors
+def runTranspose():
+    full_df_filepath = '/Users/pablo/Desktop/Masters/Github_Repository/Masters/Data/Complete_data/final_dataset.csv ( 106774 , 35 ) .csv'
+    full_df = pd.read_csv(full_df_filepath)
+    full_df = transposeVectors(transposeSentiments(full_df))
+
+    full_df_filepath = full_df_filepath.split('Complete_data/')[0] + 'Complete_data/final_dataset' + str(full_df.shape) + '.csv'
+
+    full_df.to_csv(full_df_filepath)
+
+#setUpTextData()
+#setUpFinancialData()
+#runR_files()
+runTranspose()
 
 #this script needs to:
 #
